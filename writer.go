8bff6193dc2ea14e1afcc0cce13c604a2ceb509b
nobodynobodyee <nobodynobodyee@gmail.com>
2023-07-17 00:02:02 +0800

    x

--- writer.go
+++ writer.go
@@ -3,8 +3,8 @@ package tlog
 type FileStoreModeT int
 
 const (
-	DailySplit    FileStoreModeT = 11 // Outputting everything to a single file
-	AppendOneFile FileStoreModeT = 12 // Outputting to specific files based on severity level.
+	DailySplit    FileStoreModeT = 1 // Outputting everything to a single file
+	AppendOneFile FileStoreModeT = 2 // Outputting to specific files based on severity level.
 )
 
 type Writer interface {
