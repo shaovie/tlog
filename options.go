afd9e265bda19473c9a6c168b37bdad94fa43ae7
shaovie <43458943@qq.com>
2023-09-01 16:33:33 +0800

    let panic out of Option func

--- options.go
+++ options.go
@@ -60,20 +60,20 @@ func SetWriter(w Writer) Option {
 
 // Set prefix `time` format
 func TimeFormat(v int) Option {
+	if v < 1 {
+		panic("tlog:TimeFormat param is illegal")
+	}
 	return func(o *Options) {
-		if v < 1 {
-			panic("tlog:TimeFormat param is illegal")
-		}
 		o.timeFormat = v
 	}
 }
 
 // json/text
 func Format(v int) Option {
+	if v != FormatJson && v != FormatText {
+		panic("tlog:Format param is illegal")
+	}
 	return func(o *Options) {
-		if v != FormatJson && v != FormatText {
-			panic("tlog:Format param is illegal")
-		}
 		o.format = v
 	}
 }
@@ -87,45 +87,45 @@ func OmitEmpty(v bool) Option {
 
 // for output file
 func LogDir(v string) Option {
+	if len(v) == 0 {
+		panic("tlog:LogDir param is illegal")
+	}
 	return func(o *Options) {
-		if len(v) == 0 {
-			panic("tlog:LogDir param is illegal")
-		}
 		o.logDir = v
 	}
 }
 func LogFilePrefix(v string) Option {
+	if len(v) == 0 {
+		panic("tlog:LogFilePrefix param is illegal")
+	}
 	return func(o *Options) {
-		if len(v) == 0 {
-			panic("tlog:LogFilePrefix param is illegal")
-		}
 		o.logFilePrefix = v
 	}
 }
 func FileStoreMode(v FileStoreModeT) Option {
+	if v < 1 {
+		panic("tlog:FileStoreMode param is illegal")
+	}
 	return func(o *Options) {
-		if v < 1 {
-			panic("tlog:FileStoreMode param is illegal")
-		}
 		o.fileStoreMode = v
 	}
 }
 
 // for simple post
 func PostUrl(v string) Option {
+	if len(v) == 0 {
+		panic("tlog:PostUrl param is illegal")
+	}
 	return func(o *Options) {
-		if len(v) == 0 {
-			panic("tlog:PostUrl param is illegal")
-		}
 		o.postUrl = v
 	}
 }
 
 func AnyMarshalFunc(f AnyMarshalFuncT) Option {
+	if f == nil {
+		panic("tlog:AnyMarshalFuncT param is illegal")
+	}
 	return func(o *Options) {
-		if f == nil {
-			panic("tlog:AnyMarshalFuncT param is illegal")
-		}
 		o.anyMarshalFunc = f
 	}
 }
